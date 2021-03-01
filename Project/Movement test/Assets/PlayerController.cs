7c4e535b4ccf5999c42518a6abd4d556282e6a99
Kale Abelgas <79468220+kaleabelgas@users.noreply.github.com>
2021-03-01 12:06:33 +0800

    finalize dash
    improve the dash controls and used rb.addforce for all forces

--- Project/Movement test/Assets/PlayerController.cs
+++ Project/Movement test/Assets/PlayerController.cs
@@ -5,52 +5,69 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rb;
-    public float speed = 0f;
+    public float speed = 1f;
     private Vector2 targetVelocity = Vector2.zero;
     public float dashforce = 50f;
-    private Vector2 facing = Vector2.zero;
+    private Vector2 facing = Vector2.down;
     private bool dashing = false;
+    public float dashTime = 10f;
+    public float dashCDDefault;
+    private float dashCD;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        dashCD = dashCDDefault;
+
     }
 
 
     private void Update()
     {
 
-        targetVelocity = new Vector2(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical")) * speed;
+        targetVelocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        dashCD -= Time.deltaTime;
 
-        if  (targetVelocity.x != 0  || targetVelocity.y != 0)
+        if  (Mathf.Abs(targetVelocity.x) > 0.5  || Mathf.Abs(targetVelocity.y) > 0.5)
         {
-            facing = new Vector2 (Mathf.Clamp(targetVelocity.x, -1, 1), Mathf.Clamp(targetVelocity.y, -1, 1));
-            Debug.Log(facing);
+            facing = targetVelocity;
         }
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && dashCD <= 0)
         {
             dashing = true;
             Debug.Log("Jumped!" + facing);
         }
-
     }
 
     private void FixedUpdate()
     {
         if (targetVelocity.x != 0 || targetVelocity.y != 0 && !dashing)
         {
-            rb.velocity = targetVelocity * Time.deltaTime;
+            rb.AddForce(targetVelocity * speed * Time.deltaTime, ForceMode2D.Impulse);
         }
 
         if (dashing)
         {
-            rb.velocity = facing * dashforce;
+            StartCoroutine(Dash());
             dashing = false;
         }
 
+        Debug.Log("Velocity" + rb.velocity);
+        Debug.Log("Direction" + facing);
 
+    }
 
+    IEnumerator Dash()
+    {
+        float startTime = Time.time;
+
+        while (Time.time < startTime + dashTime)
+        {
+            rb.AddForce(facing * dashforce, ForceMode2D.Impulse);
+            dashCD = dashCDDefault;
+            yield return null;
+        }
     }
 
 }
