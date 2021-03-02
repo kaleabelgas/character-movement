55b0707e4280302c021328002a478fac764367a7
Kale Abelgas <79468220+kaleabelgas@users.noreply.github.com>
2021-03-02 13:55:12 +0800

    finalize movement script

--- Project/Movement test/Assets/PlayerController.cs
+++ Project/Movement test/Assets/PlayerController.cs
@@ -6,13 +6,14 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rb;
     public float speed = 1f;
-    private Vector2 targetVelocity = Vector2.zero;
-    public float dashforce = 50f;
-    private Vector2 facing = Vector2.down;
-    private bool dashing = false;
+    public float dashForce = 50f;
     public float dashTime = 10f;
     public float dashCDDefault;
+
     private float dashCD;
+    private Vector2 facing = Vector2.down;
+    private Vector2 targetDirection = Vector2.zero;
+    private bool dashing = false;
 
     private void Awake()
     {
@@ -25,13 +26,24 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
 
-        targetVelocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        dashCD -= Time.deltaTime;
-
-        if  (Mathf.Abs(targetVelocity.x) > 0.5  || Mathf.Abs(targetVelocity.y) > 0.5)
+        targetDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        
+        if (dashCD > 0)
         {
-            facing = targetVelocity;
+            dashCD -= Time.deltaTime;
+
         }
+
+        Debug.Log(dashCD);
+
+
+        if (Mathf.Abs(targetDirection.x) > 0.5f || Mathf.Abs(targetDirection.y) > 0.5f)
+        {
+            facing = targetDirection;
+            //Debug.Log("Setting both " + facing);
+        }
+
+
 
         if (Input.GetButtonDown("Jump") && dashCD <= 0)
         {
@@ -42,19 +54,18 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (targetVelocity.x != 0 || targetVelocity.y != 0 && !dashing)
+        if (dashCD < dashTime)
         {
-            rb.AddForce(targetVelocity * speed * Time.deltaTime, ForceMode2D.Impulse);
+            Move();
         }
 
         if (dashing)
         {
             StartCoroutine(Dash());
-            dashing = false;
         }
 
-        Debug.Log("Velocity" + rb.velocity);
-        Debug.Log("Direction" + facing);
+        //Debug.Log("Velocity" + rb.velocity);
+        //Debug.Log("Direction" + facing);
 
     }
 
@@ -64,10 +75,21 @@ public class PlayerController : MonoBehaviour
 
         while (Time.time < startTime + dashTime)
         {
-            rb.AddForce(facing * dashforce, ForceMode2D.Impulse);
+            rb.AddForce(facing * dashForce * Time.deltaTime, ForceMode2D.Impulse);
+            //transform.Translate(Vector2.Lerp(facing * dashforce, transform.position, Time.deltaTime));
             dashCD = dashCDDefault;
             yield return null;
         }
+        dashing = false;
+        yield return null;
+
+    }
+
+
+    private void Move()
+    {
+        rb.velocity = speed * Time.deltaTime * targetDirection;
+        //transform.Translate(Vector2.Lerp(targetDirection * speed, transform.position, Time.deltaTime));
     }
 
 }
